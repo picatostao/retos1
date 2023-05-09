e6538963712afc049188796b6160c2ad56e5a55b
picatostao <valver123@gmail.com>
2023-05-09 11:44:36 +0200

    condicionales

--- condicionales.ts
+++ condicionales.ts
@@ -122,6 +122,10 @@ function continent(country:string){
 function isEven(num:number){
     if (num%2==0){
         console.log(num,"es par")
+    }else{
+        console.log(num,"es impar")
     }
 }
+
+
 
