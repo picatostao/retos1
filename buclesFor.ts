62029e706fde08aa4122b981604fd65427d0929a
picatostao <valver123@gmail.com>
2023-05-09 12:25:16 +0200

    buclesFor

---
+++ buclesFor.ts
@@ -0,0 +1,51 @@
+  ////////////////////////
+ //     1-impares      //
+////////////////////////
+
+function evenNumbers(num:number){
+    for(let i=0;i<=num;i++){
+        if(i%2!==0){
+            console.log(i)
+        }
+    }
+}
+
+  ////////////////////////
+ // 2 array revertido  //
+////////////////////////
+
+function myRevert(myArr:[]):any[]{ //no me deja poner :any en myArr
+    let reverse=[]
+    for(let i=myArr.length-1;i>=0; i--){
+        reverse.push(myArr[i]);
+    }
+    return myArr
+}
+
+
+  ////////////////////////
+ //     4 arcoiris     //
+////////////////////////
+
+function isRainbow(colors:string[]){
+    let arcoiris=["rojo","naranja","amarillo","verde","añil", "azul", "violeta"]
+    for (let color of colors){
+        if (arcoiris.includes(color)){
+            console.log("el ",color, " está en el arcoiris")
+        }else{
+            console.log("el ",color, " NO está en el arcoiris")
+        }
+    }
+}
+
+  //////////////////////////
+ // 5 suma de caracteres //
+//////////////////////////
+
+function add(myWords:string[]):number{
+    let sum=0
+    for (let letras of myWords){
+        sum += letras.length
+    }
+    return sum
+}
